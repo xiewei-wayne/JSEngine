41b8ea6cd40dbfbb243f48c4c9e01c1d722d6edf
tracy-e <tracy.cpp@gmail.com>
2012-11-12 21:47:03 +0800

    Initial Commit

---
+++ JSEngine/ESAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ESAppDelegate.h
+//  JSEngine
+//
+//  Created by Tracy E on 12-11-12.
+//  Copyright (c) 2012年 Tracy E. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
